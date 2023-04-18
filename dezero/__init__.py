59ce6f9d2c849cc924f4e74e2ef04945dcd22d1f
Tamashoo <tamasho-tomoya-ch@ynu.jp>
2023-04-18 14:33:26 +0900

    4/18

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,4 +1,4 @@
-is_simple_core = True
+is_simple_core = False
 
 if is_simple_core:
     from dezero.core_simple import Variable
@@ -11,15 +11,16 @@ if is_simple_core:
 
 else:
     from dezero.core import Variable
-    from dezero.core import Parameter
+    #from dezero.core import Parameter
     from dezero.core import Function
     from dezero.core import using_config
     from dezero.core import no_grad
-    from dezero.core import test_mode
+    #from dezero.core import test_mode
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
     from dezero.core import Config
+    """
     from dezero.layers import Layer
     from dezero.models import Model
     from dezero.datasets import Dataset
@@ -32,9 +33,10 @@ else:
     import dezero.functions
     import dezero.functions_conv
     import dezero.layers
-    import dezero.utils
     import dezero.cuda
     import dezero.transforms
+    """
+    import dezero.utils
 
 setup_variable()
 __version__ = '0.0.13'
