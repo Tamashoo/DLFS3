fd2a6554012c7f793a4abed9f61268e9bc328532
Tamashoo <tamasho-tomoya-ch@ynu.jp>
2023-04-19 14:52:30 +0900

    4/19

--- dezero/utils.py
+++ dezero/utils.py
@@ -70,3 +70,14 @@ def plot_dot_graph(output, verbose=True, to_file='graph.png'):
         return display.Image(filename=to_file)
     except:
         pass
+
+def sum_to(x, shape):
+    ndim = len(shape)
+    lead = x.ndim - ndim
+    lead_axis = tuple(range(lead))
+
+    axis = tuple([i + lead for i, sx in enumerate(shape) if sx == 1])
+    y = x.sum(lead_axis + axis, keepdims=True)
+    if lead > 0:
+        y = y.squeeze(lead_axis)
+    return y
